726f62f39483f4e188612d798faf98cd73545b8f
vishnukgcherupuzha@gmail.com <vishnukgcherupuzha@gmail.com>
2019-12-18 14:07:20 +0530

    Updates

---
+++ precache-manifest.6516d074a27c8256aaaaa39cca137be2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa91ae4ebd9a914efb151a69a106722",
+    "url": "/vishnukgcherupuzha/Cubicom/index.html"
+  },
+  {
+    "revision": "755771f48c34d2a65a26",
+    "url": "/vishnukgcherupuzha/Cubicom/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f32a72e136c9746eac87",
+    "url": "/vishnukgcherupuzha/Cubicom/static/css/main.a7bbed6a.chunk.css"
+  },
+  {
+    "revision": "755771f48c34d2a65a26",
+    "url": "/vishnukgcherupuzha/Cubicom/static/js/2.4a9cc418.chunk.js"
+  },
+  {
+    "revision": "62089eb17da7fdb2a0c3934ab93c6e32",
+    "url": "/vishnukgcherupuzha/Cubicom/static/js/2.4a9cc418.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f32a72e136c9746eac87",
+    "url": "/vishnukgcherupuzha/Cubicom/static/js/main.4e555282.chunk.js"
+  },
+  {
+    "revision": "26e98ac874e085eab4a5",
+    "url": "/vishnukgcherupuzha/Cubicom/static/js/runtime-main.d1257e1b.js"
+  },
+  {
+    "revision": "3de23a32ce9fec81b3ce4ae3a6e7cfc1",
+    "url": "/vishnukgcherupuzha/Cubicom/static/media/Background.3de23a32.png"
+  },
+  {
+    "revision": "bee00f6fbcfb01efb642cfc69d00f17d",
+    "url": "/vishnukgcherupuzha/Cubicom/static/media/back.bee00f6f.png"
+  }
+]);
